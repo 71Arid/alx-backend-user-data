cbe566ec44c7420560b3d9caea5e2cc7f32cd24b
71Arid <no232el@gmail.com>
2024-04-22 11:46:21 +0300

    filtered_logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+fields: a list of strings representing
+all fields to obfuscate
+redaction: a string representing by what the
+field will be obfuscated
+message: a string representing the log line
+separator: a string representing by which character
+is separating all fields in the log line (message)
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    return re.sub(r'({})=[^{}]+'.format('|'.join(fields), separator), r'\1=' + redaction, message)
