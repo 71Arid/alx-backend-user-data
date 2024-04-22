83774578b5cfe0d8159f2545c8ff8278d8679baf
71Arid <no232el@gmail.com>
2024-04-22 14:17:25 +0300

    filtered_logger documented function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,12 +9,28 @@ separator: a string representing by which character
 is separating all fields in the log line (message)
 """
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
     """1-main.py should use a regex to replace
     occurrences of certain field values.
-    """
+        """
     return re.sub(r'({})=[^{}]+'.format(
         '|'.join(fields), separator), r'\1=' + redaction, message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
