a8ded19aefaf93f2941390c12c653cd870f1a409
71Arid <no232el@gmail.com>
2024-04-28 21:19:30 +0300

    app

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,7 +27,7 @@ def before_request():
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth is None:
         return
-    if auth.require_auth(request.path, excluded_paths):
+    if not auth.require_auth(request.path, excluded_paths):
         return
     if not auth.authorization_header(request):
         abort(401)
