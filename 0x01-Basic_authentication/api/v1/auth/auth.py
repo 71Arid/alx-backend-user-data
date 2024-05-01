94b3bc7fd3972d8724d94850b1d32a9f47ff6047
71Arid <no232el@gmail.com>
2024-05-01 21:35:54 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ a class to manage the API authentication.
 """
 from flask import request
 from typing import List, TypeVar
+from fnmatch import fnmatch
 
 
 class Auth():
@@ -18,8 +19,9 @@ class Auth():
             return True
         if path[last_index] != "/":
             path = path + "/"
-        if path in excluded_paths:
-            return False
+        if excluded_path in excluded_paths:
+            if fnmatch(path, excluded_path):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
