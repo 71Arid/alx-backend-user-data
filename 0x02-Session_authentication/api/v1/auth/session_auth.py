bc3a6cc46af66cdb4cb9fb3d7b5d203bea35ad08
71Arid <no232el@gmail.com>
2024-05-03 21:23:11 +0300

    creating session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,9 +6,19 @@ Its the first step for creating
 a new authentication mechanism:
 """
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     class to implement session auth logic
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None or not type(user_id) == str:
+            return None
+        sess_id = uuid4()
+        self.user_id_by_session_id[sess_id] = user_id
+        return sess_id
