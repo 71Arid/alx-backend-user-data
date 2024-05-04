a6f6aeea560c6f63a6fabce9f38351fd54e795a4
71Arid <no232el@gmail.com>
2024-05-04 06:14:19 +0300

    session id for identifying a user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ a new authentication mechanism:
 """
 from .auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -29,3 +30,10 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value"""
+        sess_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(sess_id)
+        user = User.get(user_id)
+        return user
