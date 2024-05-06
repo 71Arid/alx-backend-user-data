2a9fcb0b7563fd1e2c616157e2a54f606d6677e3
71Arid <no232el@gmail.com>
2024-05-06 06:05:03 +0300

    user route added

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,16 +2,34 @@
 """
 Basic flask app implementation
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+
+
+AUTH = Auth()
 
 
 @app.route('/')
 def index():
     """returns a jsonify payload"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register_user():
+    """registers a user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        msg = {"email": user.email, "message": "user created"}
+        return jsonify(msg)
+    except Exception:
+        msg = {"message": "email already registered"}
+        return jsonify(msg), 400
 
 
 if __name__ == "__main__":
