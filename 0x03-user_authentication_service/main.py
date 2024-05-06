430cb0806bca9fe9d069ca07b609a5d1eaed1949
71Arid <no232el@gmail.com>
2024-05-06 20:44:09 +0300

    intergration testing

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -9,6 +9,7 @@ import requests
 
 BASE_URL = "http://localhost:5000"
 
+
 def register_user(email: str, password: str) -> None:
     """Registers a user."""
     url = f"{BASE_URL}/register"
@@ -16,12 +17,14 @@ def register_user(email: str, password: str) -> None:
     response = requests.post(url, json=payload)
     assert response.status_code == 200
 
+
 def log_in_wrong_password(email: str, password: str) -> None:
     """Logs in with wrong password."""
     url = f"{BASE_URL}/login"
     payload = {"email": email, "password": password}
     response = requests.post(url, json=payload)
     assert response.status_code == 401
+
 
 def log_in(email: str, password: str) -> str:
     """Logs in and returns session ID."""
@@ -31,11 +34,13 @@ def log_in(email: str, password: str) -> str:
     assert response.status_code == 200
     return response.cookies.get("session_id")
 
+
 def profile_unlogged() -> None:
     """Accesses profile page while unlogged."""
     url = f"{BASE_URL}/profile"
     response = requests.get(url)
     assert response.status_code == 403
+
 
 def profile_logged(session_id: str) -> None:
     """Accesses profile page while logged."""
@@ -44,12 +49,14 @@ def profile_logged(session_id: str) -> None:
     response = requests.get(url, cookies=cookies)
     assert response.status_code == 200
 
+
 def log_out(session_id: str) -> None:
     """Logs out."""
     url = f"{BASE_URL}/sessions"
     cookies = {"session_id": session_id}
     response = requests.delete(url, cookies=cookies)
     assert response.status_code == 302
+
 
 def reset_password_token(email: str) -> str:
     """Requests a reset password token."""
@@ -59,12 +66,14 @@ def reset_password_token(email: str) -> str:
     assert response.status_code == 200
     return response.json()["reset_token"]
 
+
 def update_password(email: str, reset_token: str, new_password: str) -> None:
     """Updates password using reset token."""
     url = f"{BASE_URL}/reset_password/{reset_token}"
     payload = {"email": email, "new_password": new_password}
     response = requests.put(url, json=payload)
     assert response.status_code == 200
+
 
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
