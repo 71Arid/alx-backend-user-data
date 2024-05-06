ad3362cfa47437492494632260b9c6d4ef313053
71Arid <no232el@gmail.com>
2024-05-06 09:34:22 +0300

    get_reset_password_token function in auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,3 +79,13 @@ class Auth:
             return
         self._db.update_user(user_id, session_id=None)
         return
+
+    def get_reset_password_token(self, email: str) -> str:
+        """generates reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
